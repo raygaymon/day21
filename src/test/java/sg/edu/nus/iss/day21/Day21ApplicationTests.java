80feace1b6afb99e060bf7c89bfbfd6bbd283199
raygaymon <raysonlim.jr@gmail.com>
2023-06-20 12:25:41 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/day21/Day21ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
